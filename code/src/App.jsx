630711101b3fe11d9df814241a3a512cf4d333f0
“noa <noaavizror@gmail.com>
2025-03-18 22:24:33 +0200

    עשינו קומפוננטות לכל אחד מהטפסים

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Header from './component/Header'
+import Event from './component/Event'
 import { Route, Routes } from 'react-router-dom'
-import Home from './component/home'
-
+import Courses from './component/Courses'
+import Info from './component/Info'
+import StudentInfo from './component/StudentInfo'
+import Task from './component/Task'
+import Home from './component/Home'
 export default function App() {
   return (
     <div>
 
 <Header />
  <Routes>
- <Route path='/' element={<Home />} />
- 
+ <Route path='/Home' element={<Home />} />
+ <Route path='/Courses' element={<Courses/>} />
+ <Route path='/Event' element={<Event />} />
+ <Route path='/Header' element={<Header />} />
+ <Route path='/Info' element={<Info />} />
+ <Route path='/StudentInfo' element={<StudentInfo />} />
+ <Route path='/Task' element={<Task />} />
  </Routes>
 
 
---
+++ code/src/component/StudentInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudentInfo() {
+  return (
+    <div>StudentInfo</div>
+  )
+}
